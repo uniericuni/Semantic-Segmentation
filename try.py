1f6ab7d0a92a285e63020866ac777ae4bb052895
Uniericuni <uniericuni@gmail.com>
2017-03-05 17:19:20 -0500

    [Add] try.py as main program

---
+++ try.py
@@ -0,0 +1,54 @@
+# Reference: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/tutorials/mnist/mnist_softmax.py
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+import tensorflow as tf
+
+FLAG = None
+
+def main():
+
+    # imort data
+    # TODO: the following line
+    pascal = read_pascal()
+
+    # Create the model
+    # TODO: define dim, outDim
+    x = tf.placeholder(tf.float32, shape=[None, dim]) #shape=[batch size, dimemsionality] 
+    # W = tf.Variable(tf.zeros([dim, outDim]))
+    # b = tf.Variable(tf.zeros([outDim]))
+    y = tf.matmul(x,W)+b
+
+    # Define loss and optimizer
+    # TODO: assign inference, logits=output of inference
+    y_ = tf.placeholder(tf.float32, [None, outDim])
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+    train_step = tf.train.GradientDescentOptimizer(0.5).mimize(cross_entropy)
+
+    # Session Define
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+
+    # Training
+    # TODO: define MAX_ITER, BATCH_SIZE
+    for _ in range(MAX_ITER):
+        batch_xs, batch_ys = pascal.train.next_batch(BATCH_SIZE)
+        sess.run(train_step, feed_dict={x: batch_batch_xs, y_: batch_ys}
+
+    # Testing
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accuracy, feed_dict={x: pascal.test.images,
+                                        y_: mnist.test.labels})
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    # TODO: the following line
+    parser.add_argument('--data_dir', type=str, default=route, help='Directory for storing data')
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]]+unparsed)
